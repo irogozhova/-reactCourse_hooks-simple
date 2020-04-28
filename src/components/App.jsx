e3d9dd1ebdef5d0e82b6605344441381986df61b
Irina Rogozhova <irina.rogozhova@saltpepper.ru>
2020-04-28 18:51:07 +0300

    Added app that fetches different lists of data from API based on props with useState and useEffect hooks

---
+++ src/components/App.jsx
@@ -0,0 +1,27 @@
+import React, { useState }from 'react';
+import ResourceList from './ResourceList';
+
+const App = () => {
+  const [resource, setResource] = useState('posts');
+  // resource - current value of this piece of state - could be given any name; smth like this.state.currentValue
+  // setResource - a setter function to call when we want to update our state (and rerender); like this.setState()
+  // 'posts' - starting value for this piece of state
+
+  // const [currentCount, setCount] = useState(0);
+
+  return (
+    <div>
+      <div>
+        <button onClick={() => setResource('posts')}>Posts</button>
+        <button onClick={() => setResource('todos')}>Todos</button>
+      </div>
+      <ResourceList resource={resource}/>
+      {/* <div>
+        <button onClick={() => setCount(currentCount + 1)}>Increment</button>
+        {currentCount}
+      </div> */}
+    </div>
+  )
+}
+
+export default App;
---
+++ src/components/ResourceList.jsx
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const ResourceList = ({ resource }) => {
+  const [resources, setResources] = useState([]);
+
+  const fetchResource = async (resource) => {
+    const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`);
+   
+    setResources(response.data);
+  }
+
+  useEffect(
+    () => { // runs every time when component renders to the screen or updates, i.e. after render
+      fetchResource(resource)
+    }, 
+    [resource] // if empty array is passed here, it's like componentDidMount (function is invoked only when component is first rendered)
+    // and if nothing is passed - it will run the 'effect' function on every render
+  )
+
+  return <ul>{resources.map(record => <li key={record.id}>{record.title}</li>)}</ul>
+}
+
+export default ResourceList;
